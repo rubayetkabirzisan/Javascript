99792e4e43b41b8dd330340827640aa1415f307e
rubayetkabirzisan <rubayetkabirz@gmail.com>
2025-11-28 23:32:38 +0600

    2 done

--- Day 2/script.js
+++ Day 2/script.js
@@ -1,5 +1,6 @@
-// Variable in Javascript
-
+// Variables in Javascript
+console.log("Variables in JavaScript")
+// var, let and const are used to declare variables in javascript
 let a = 67// a contains 67
 console.log(a)
 a = "harry"
