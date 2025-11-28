a9d23e25efde1bab145326905f080efd17d4fa8d
rubayetkabirzisan <rubayetkabirz@gmail.com>
2025-11-28 23:00:17 +0600

    Updated day 1

--- Day 1/script.js
+++ Day 1/script.js
@@ -1,2 +1,10 @@
 // Introduction to programming in JavaScript
 console.log("Hello, World!");
+
+// Function to add two numbers
+console.log("Adding three numbers");
+function addThreeNumbers(a, b, c) {
+    return a + b + c;
+}
+let sum = addThreeNumbers(5, 10, 15);
+console.log("Sum of three numbers:", sum);
