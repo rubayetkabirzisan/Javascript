35c93fb2264dfcbfa7577681e7910fbb17d4cb4a
rubayetkabirzisan <rubayetkabirz@gmail.com>
2025-11-28 23:35:56 +0600

    Day 4

---
+++ Day 4/script.js
@@ -0,0 +1,15 @@
+// Primitives and Objects in JavaScript
+// There are 7 primitive data types in JavaScript:
+// 1. null
+// 2. number
+// 3. symbol
+// 4. string
+// 5. boolean
+// 6. bigint
+// 7. undefined
+// Everything else is an object.
+// Primitives are immutable, meaning their values cannot be changed.
+// Objects are mutable, meaning their properties can be changed.
+
+
+
